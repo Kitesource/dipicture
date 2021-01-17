a9aab772deb3d4118b1e597b6e460a0787321cfd
kite <2257440643@qq.com>
2021-01-17 22:29:13 +0800

    finish home-recommend

---
+++ src/pages/home/home-recommend/index.vue
@@ -0,0 +1,212 @@
+<template>
+  <scroll-view
+    v-if="recommends.length > 0"
+    class="recommend_view"
+    scroll-y
+    @scrolltolower="handleToLower"
+  >
+    <!-- 推荐区域 -->
+    <view class="recommend_wrap">
+      <view class="recommend_item" v-for="item in recommends" :key="item.id">
+        <image mode="widthFix" :src="item.thumb"></image>
+      </view>
+    </view>
+
+    <!-- 月份 -->
+    <view class="months_wrap">
+      <view class="months_title">
+        <view class="months_title_info">
+          <view class="months_info">
+            <text> {{ months.DD }}/ </text>
+            {{ months.MM }} 月
+          </view>
+          <view class="months_text">{{ months.title }}</view>
+        </view>
+        <view class="months_title_more">更多 > </view>
+      </view>
+      <view class="months_content">
+        <view class="months_item" v-for="item in months.items" :key="item.id">
+          <image
+            mode="aspectFill"
+            :src="item.thumb + item.rule.replace('$<Height>', 360)"
+          ></image>
+        </view>
+      </view>
+    </view>
+
+    <!-- 热门 -->
+    <view class="hots_wrap">
+      <view class="hots_title">
+        <text>热门</text>
+      </view>
+      <view class="hots_content">
+        <view class="hots_item" v-for="item in hots" :key="item.id">
+          <image :src="item.thumb" mode="widthFix" />
+        </view>
+      </view>
+    </view>
+  </scroll-view>
+</template>
+
+<script>
+import moment from "moment";
+export default {
+  data() {
+    return {
+      //推荐列表
+      recommends: [],
+      //月份
+      months: {},
+      //热门
+      hots: [],
+      //请求的参数
+      params: {
+        //要获取几条数据
+        limit: 30,
+        //关键字
+        order: "hot",
+        //要跳过几条
+        skip: 0
+      },
+      //表示是否还有下一页
+      hasMore:true
+    };
+  },
+  //组件挂载完成时触发
+  mounted() {
+    this.getList();
+  },
+  methods: {
+    //获取接口数据
+    getList() {
+      this.request({
+        url: "http://157.122.54.189:9088/image/v3/homepage/vertical",
+        data: this.params
+      }).then(result => {
+        console.log(result);
+
+
+        //判断还有没有下一页数据
+        if(result.res.vertical.length === 0) {
+          this.hasMore = false;
+          return;
+        }
+
+        if(this.recommends.length === 0) { //第一次发送请求
+          //推荐模块
+        this.recommends = result.res.homepage[1].items;
+        //月份模块
+        this.months = result.res.homepage[2];
+        // 将时间戳 改成 18号/月 moment.js
+        this.months.MM = moment(this.months.stime).format("MM");
+        this.months.DD = moment(this.months.stime).format("DD");
+        }
+
+        //热门数据列表
+        //数组拼接 es6
+        this.hots = [...this.hots, ...result.res.vertical];
+      });
+    },
+
+    //滚动条触底事件
+    handleToLower() {
+      // console.log("111");
+      /* 
+        1 修改参数 skip += limit;
+        2 重新放请求 getList() 
+        3 请求回来的 hots 数据叠加
+      */
+     if(this.hasMore) {
+       this.params.skip += this.params.limit;
+       this.getList();
+     }else {
+       //弹窗提示
+       uni.showToast({
+         title: '没有数据了',
+         icon: 'none',
+         mask: true
+       })
+     }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.recommend_view {
+  //height = 屏幕高度 - 头部标题高度
+  height: calc(100vh - 36rpx);
+}
+
+.recommend_wrap {
+  display: flex;
+  flex-wrap: wrap;
+  .recommend_item {
+    width: 50%;
+    border: 5rpx solid #fff;
+  }
+}
+
+//月份
+.months_wrap {
+  .months_title {
+    display: flex;
+    justify-content: space-between;
+    padding: 20rpx;
+    .months_title_info {
+      color: $color;
+      font-size: 30rpx;
+      font-weight: 600;
+      display: flex;
+      .months_info {
+        text {
+          font-size: 36rpx;
+        }
+      }
+
+      .months_text {
+        font-size: 34rpx;
+        color: #666;
+        margin-left: 30rpx;
+      }
+    }
+
+    .months_title_more {
+      font-size: 24rpx;
+      color: $color;
+    }
+  }
+
+  .months_content {
+    display: flex;
+    flex-wrap: wrap;
+    .months_item {
+      width: 33.33%;
+      border: 5rpx solid #fff;
+    }
+  }
+}
+
+.hots_wrap {
+  .hots_title {
+    padding: 20rpx;
+    text {
+      border-left: 5px solid $color;
+      padding-left: 20rpx;
+      font-size: 34rpx;
+      font-weight: 600;
+    }
+  }
+
+  .hots_content {
+    display: flex;
+    flex-wrap: wrap;
+    .hots_item {
+      width: 33.33%;
+      border: 5rpx solid #fff;
+      image {
+      }
+    }
+  }
+}
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,15 +1,90 @@
 <template>
-  <view>首页
-    <text class="iconfont icon-shoucang"></text>
+  <view>
+    <view class="home_tab">
+      <view class="home_tab_title">
+        <view class="title_inner">
+          <uni-segmented-control
+            :current="current"
+            :values="items.map(v => v.title)"
+            @clickItem="onClickItem"
+            style-type="text"
+            active-color="#d4237a"
+          ></uni-segmented-control>
+        </view>
+        <view class="iconfont icon-search"></view>
+      </view>
+      <view class="home_tab_content">
+        <view v-if="current === 0">
+          <home-recommend></home-recommend>
+        </view>
+        <view v-if="current === 1">
+          <home-category></home-category>
+        </view>
+        <view v-if="current === 2">
+          <home-new></home-new>
+        </view>
+        <view v-if="current === 3">
+          <home-album></home-album>
+        </view>
+      </view>
+    </view>
   </view>
 </template>
 
 <script>
-export default {
+import { homeAlbum } from "./home-album";
+import { homeCategory } from "./home-category";
+import { homeNew } from "./home-new";
+import { homeRecommend } from "./home-recommend";
 
-}
+// 引入分段器组件
+import { uniSegmentedControl } from "@dcloudio/uni-ui";
+export default {
+  components: {
+    homeAlbum,
+    homeCategory,
+    homeNew,
+    homeRecommend,
+    uniSegmentedControl
+  },
+  data() {
+    return {
+      items: [
+        { title: "推荐" },
+        { title: "分类" },
+        { title: "最新" },
+        { title: "专辑" }
+      ],
+      current: 0
+    };
+  },
+  methods: {
+    onClickItem(e) {
+      if (this.current !== e.currentIndex) {
+        this.current = e.currentIndex;
+      }
+    }
+  },
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.home_tab {
+  .home_tab_title{
+    position: relative;
+    .title_inner{
+      width: 60%;
+      margin: 0 auto;
+    }
+    .icon-search {
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      right: 5%;
+    }
+  }
+  .home_tab_content{
 
+  }
+}
 </style>
